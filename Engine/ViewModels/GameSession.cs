0c5f666b7d1bb2de2df3f7cca24db58e2eab80e3
Abraham Gomez <agomez717@gmail.com>
2017-08-08 22:33:13 -0700

    Added more sections and labels

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public Location CurrentLocation { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Abe";
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.CharacterClass = "Programmer";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house.";
+
+        }
+    }
+}
